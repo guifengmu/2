3a1b6320d9cdffc9477f7bce095cf23563eb009d
guifengmu <39817491+guifengmu@users.noreply.github.com>
2018-07-05 16:14:12 +0800

    Add files via upload

---
+++ canady.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from atexit import register
+from random import randrange
+from threading import BoundedSemaphore,Lock,Thread
+from time import sleep,ctime
+
+lock = Lock()
+MAX = 5
+candytray = BoundedSemaphore(MAX)
+
+def refill():
+	lock.acquire() 
+	print('Refilling candy...')
+	try:
+		candytray.release() 
+
+		print('OK')
+	except ValueError:
+		print('full,skiping')
+	lock.release() 
+
+
+def buy():
+	lock.acquire()
+	print('Buying candy...')
+	if candytray.acquire(False):
+		print('Ok')
+	else:
+		print('empty,skiping')
+	lock.release()
+
+def produce(loops):
+	for i in range(loops):
+		refill()
+		sleep(randrange(3))
+
+def consumer(loops):
+	for i in range(loops):
+		buy()
+		sleep(randrange(3))
+
+def main():
+	print('Starting at:',ctime())
+	nloops = randrange(2,6)
+	print('The candy machine (full with %d bars)!' % MAX)
+	Thread(target = consumer,args=(randrange(nloops,nloops+MAX+2),)).start()
+	Thread(target = produce,args = (nloops,)).start()
+
+@register
+def _atexit():
+	print('all DONE at:',ctime())
+
+if __name__ == '__main__':
+	main()
